7fca4a1933fb8acaf3c4a3f921c61493113f37c6
Aimos320 <amosemma93@gmail.com>
2022-04-27 13:46:03 +0100

    added to the about and contact components

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,9 +5,10 @@ import "./Home.css";
 const Home = () => {
   return <div id="main" ><div className="home">
     <h3> It's A Great Time For Your Taste Buds </h3>
-    <h1><span>MEAL</span> FOR <br/> WEEK </h1>
+    <h1><span>MEAL</span> FOR <br /> WEEK </h1>
     <div className="
     header-btns"><a href="#" className="header-btn"> Order Now</a></div>
+    
   </div>;
   </div>;
 };
